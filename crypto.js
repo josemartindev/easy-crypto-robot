b0453f5b6031366b9d1833f8fd4a199a9e4c35a6
Jose Martin <43208594+josemartindev@users.noreply.github.com>
2021-03-30 15:43:28 +0200

    Crypto Robot

---
+++ crypto.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+const colors = require('colors');
+const url_BTC = "https://data.messari.io/api/v1/assets/btc/metrics"
+const eur = 0.85202852 // Euro's price of 30/03/2021
+
+const getData = async () => {
+    console.log("Crypto Robot:".underline.yellow, "🕒 " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds());
+    await axios.get(url_BTC, { headers: { "x-messari-api-key": "" }
+    }).then((res) => {
+        let btc = res.data.data.market_data.price_usd * eur;
+        console.log("BTC: ".green, btc.toString().substr(0, 8).magenta);
+    }).catch((err) => {
+        console.log("ERROR: ", err);
+    })
+}
+
+getData();
+setInterval(function () { getData(); }, 30000); // every 30 seconds
